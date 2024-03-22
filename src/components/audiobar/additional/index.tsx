04c5f2c515b768ada226c112aa00d320c50e8938
pyromatic-x <juseedboss@yandex.ru>
2024-03-22 22:57:15 +0300

    added rightbar handlers to audiobar additional buttons

--- src/components/audiobar/additional/index.tsx
+++ src/components/audiobar/additional/index.tsx
@@ -2,12 +2,17 @@ import { Grid } from '@mui/material';
 import { Airplay, Devices, Lyrics, OpenInFull, QueueMusicRounded } from '@mui/icons-material';
 import IconWithTooltip from './Icon';
 import Volume from './Volume';
+import { setRightbarContent } from '../../rightbar/effect';
 
 const Additional = () => (
   <Grid container flexWrap="nowrap" columnGap={1.35} width="max-content" justifySelf="end">
-    <IconWithTooltip tooltip="Now Playing View" Icon={Airplay} />
+    <IconWithTooltip
+      tooltip="Now Playing View"
+      Icon={Airplay}
+      onClick={() => setRightbarContent('NOW_PLAYING')}
+    />
     <IconWithTooltip tooltip="Lyrics" Icon={Lyrics} />
-    <IconWithTooltip tooltip="Queue" Icon={QueueMusicRounded} />
+    <IconWithTooltip tooltip="Queue" Icon={QueueMusicRounded} onClick={() => setRightbarContent('QUEUE')} />
     <IconWithTooltip tooltip="Connect to a device" Icon={Devices} />
     <Volume />
     <IconWithTooltip tooltip="Full screen" Icon={OpenInFull} />
