24090c2455c06b8d72e7e396dfa28fef85fc885d
pyromatic-x <juseedboss@yandex.ru>
2024-03-28 21:12:28 +0300

    added disabled attr to recently played tab

--- src/components/queue/index.tsx
+++ src/components/queue/index.tsx
@@ -3,10 +3,14 @@ import { useRef, useState } from 'react';
 import { Content, StyledButton, Tabs } from './styled';
 import QueueList from './QueueList';
 import RecentlyPlayedList from './RecentlyPlayedList';
+import { useUnit } from 'effector-react';
+import { $recentlyPlayed } from '../audiobar/effect';
 
 type ITabs = 'QUEUE' | 'RECENTLY_PLAYED';
 
 const Queue = () => {
+  const recentlyPlayed = useUnit($recentlyPlayed);
+
   const contentRef = useRef<HTMLDivElement>(null);
 
   const [activeTab, setActiveTab] = useState<ITabs>('QUEUE');
@@ -30,6 +34,7 @@ const Queue = () => {
           Queue
         </StyledButton>
         <StyledButton
+          disabled={!recentlyPlayed.length}
           active={activeTab === 'RECENTLY_PLAYED'}
           onClick={() => handleTabChange('RECENTLY_PLAYED')}
         >
